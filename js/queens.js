bccacc2c04fe08d05e04e356ec8fad490c840443
Thomas Hodges <thodges@javascriptonline.org>
2016-12-12 23:35:20 -0800

    need to fix row/column checking algorythms and backward recursion

--- js/queens.js
+++ js/queens.js
@@ -3,21 +3,60 @@ var myBoard;
 document.addEventListener('DOMContentLoaded', function() {
     myBoard = GenerateBoard();
     myBoard.drawBoard(8);
-    recursiveDrop(1);
+    recursiveTest(0);
 }, false);
 
 function recursiveDrop(j) {
+    var DROPPAUSE = 700;
+    var yDim = myBoard.getDimensionY();
+    var halfYDim = yDim/2;
     var steps = myBoard.getSize();
     var cy = d3.select("svg")
     .select("#q"+j)
     .attr("cy");
     cy = parseInt(cy);
-    cy = (cy-40)/80;
-    //alert(cy+" <= "+ steps + " : " + (cy<=steps));
+    cy = (cy-halfYDim)/yDim;
     if(cy <= steps){
         myBoard.downQueen(j);
-        setTimeout(function(){new recursiveDrop(j)}, 700);
-    };
+        setTimeout(function(){new recursiveDrop(j)}, DROPPAUSE);
+    } else {
+        myBoard.resetQueen(j);
+        setTimeout(function(){new recursiveDrop(j)}, DROPPAUSE);
+        
+    }
+
+}
+
+function recursiveTest(j) {
+    var DROPPAUSE = 700;
+    var yDim = myBoard.getDimensionY();
+    var halfYDim = yDim/2;
+    var steps = myBoard.getSize();
+    var i = getPosition();
+    
+    if(i <= steps){
+        myBoard.downQueen(j);
+        if(myBoard.check((i+1), j))
+            setTimeout(function(){new recursiveTest(j+1)}, DROPPAUSE);
+        else
+            setTimeout(function(){new recursiveTest(j)}, DROPPAUSE);
+    } else {
+        myBoard.resetQueen(j);
+        setTimeout(function(){new recursiveTest(j-1)}, DROPPAUSE);
+        
+    }
+    
+    function getPosition(){
+        var cy = d3.select("svg")
+        .select("#q"+j)
+        .attr("cy");
+        var i = parseInt(cy);
+        i = (cy-halfYDim)/yDim;
+        return i;
+        
+    }
+    
+    
 }
 
 function GenerateBoard() {
@@ -25,6 +64,7 @@ function GenerateBoard() {
     var board = [];
     var DELTA_X = 80;
     var DELTA_Y = 80;
+    var DROPTIME = 500;
     
     function getSize(){
         return this.size;
@@ -41,9 +81,11 @@ function GenerateBoard() {
     //draws a board of size size - intialises the size variable;
     function drawBoard(size){
         this.size = size;
-        for(i = 0; i< this.size; i++){
-            board[i] = [size];
-        }
+        for(j = 0; j< this.size; j++){
+            board[j] = [];
+            for(i=0; i<this.size; i++)
+                board[j][i] = 0;
+        };
         d3.select("svg").attr("style","height: "+size*DELTA_Y+"px; width: "+size*DELTA_X+"px;");
         //draw boxes
         for(i = 0; i<this.size; i++){
@@ -73,58 +115,52 @@ function GenerateBoard() {
     
     //checks if a queen in position(i,j) is safe.  returns true or false
     function check(i, j){
-        var clear = true;
+        var isClear = true;
         //check horizontal
-        for(k = 0; k<size; k++){
-            if ((board[i][k] === 1) && (k!=j)){
-                hLine(k)
-                clear = false;
-            }
-        }
-        //check vertical
-        for(k = 0; k<size; k++){
-            if ((board[k][j] === 1) && (k!=i)){
-                vLine(k)
-                clear = false;
-            }
-        }
+        for(k = 0; k<this.size; k++){
+            //console.log(board[k][i]);
+            if ((board[k][i] == 1) && (k != j)){
+                hLine(i);
+                isClear = false;
+            };
+        };
         //check diagonal right
         if(i >= j){
             var diff = i - j;
-            for(k = diff; k<size; k++){
-                if ((board[k][k-diff] === 1) && (k!=i)){
-                    //drLine(i,j)
-                    clear = false;
+            for(k = diff; k<this.size; k++){
+                if ((board[k][k-diff] == 1) && (k!=i)){
+                    drLine(i,j);
+                    isClear = false;
                 }
             }
         } else {
             var diff = j - i;
-            for(k = diff; k<size; k++){
-                if ((board[k-diff][k] === 1) && (k!=j)){
-                    //dlLine(i,j)
-                    clear = false;
-                }
-            }
-        }
-        //check diagonal left
-        if((i+j)<size){
+            for(k = diff; k<this.size; k++){
+                if ((board[k-diff][k] == 1) && (k!=j)){
+                    dlLine(i,j);
+                    isClear = false;
+                };
+            };
+        };
+/*        //check diagonal left
+        if((i+j)<this.size){
             var sum = i+j;
             for(k=0; k<=sum; k++){
-                if((board[k][sum-k] === 1) & (k!=i)){
-                    drline(i,j)
-                    clear = false;
-                }
-            }
+                if((board[k][sum-k] == 1) & (k!=i)){
+                    dlLine(i,j);
+                    isClear = false;
+                };
+            };
         } else {
-            var sum = i+j-(size-1);
-            for(k = sum; k<size; k++){
-                if((board[k][size-1-k] === 1) & (k!=i)){
-                    drline(i,j)
-                    clear = false;
-                }
-            }
-        }
-        return clear;  
+            var sum = i+j-(this.size-1);
+            for(k = sum; k<this.size; k++){
+                if((board[k][this.size-1-k] === 1) & (k!=i)){
+                    dlline(i,j);
+                    isClear = false;
+                };
+            };
+        };*/
+        return isClear;  
     }
     
     //draw a horizontal line across the board from point in row i.
@@ -196,30 +232,46 @@ function GenerateBoard() {
     }
     
     function clearLines(){
-        dr.select("svg").selectAll("line").remove();
+        d3.select("svg").selectAll("line").remove();
     }
     
     //advances queen in column j down by 1, checks validity, returns true if clear and false if not clear
     function downQueen(j){
+        clearLines();
         var cy = d3.select("svg")
         .select("#q"+ j)
         .attr("cy");       
         cy = parseInt(cy);
-        cy += 80;
-        newSlot=(cy-40)/80;
+        cy += DELTA_Y;
+        newSlot=(cy-(DELTA_Y/2))/DELTA_Y;
 
         d3.select("svg")
         .select("#q" + j)
         .transition()
-        .duration(500)
+        .duration(DROPTIME)
         .attr("cy", cy);
         
         if(newSlot > 0)
             board[j][newSlot - 1] = 0;
-        if(newSlot < size)
-            board[j][newSlot] = 1;
+        if(newSlot < this.size)
+            board[j][newSlot] = 1;  
+        //console.log("board["+j+"]["+(newSlot)+"] = "+ board[j][newSlot]);
         
+    }
+    
+    function resetQueen(j) {
+        board[j] = [];
+        d3.select("svg")
+        .select("#q"+j)
+        .remove();
         
+        d3.select("svg")
+            .append("circle")
+            .attr("id", "q"+j)
+            .attr("r", DELTA_Y/2 + "px")
+            .attr("cx", (j * DELTA_X) + (DELTA_X / 2))
+            .attr("cy", -1*(DELTA_Y / 2))
+            .style("fill", "blue");
     }
     
     return{
@@ -228,6 +280,7 @@ function GenerateBoard() {
         getDimensionX: getDimensionX,
         getDimensionY: getDimensionY,
         check: check,
-        downQueen: downQueen
+        downQueen: downQueen,
+        resetQueen: resetQueen
     }
 }
